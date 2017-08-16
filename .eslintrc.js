0a4eed6d9905d998266cc9c9d2ffb2a8f2685fe6
Rex Tsou <akccakccwww@gmail.com>
2017-08-16 22:56:11 +0800

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ],
-    "env": {
-        "browser": true,
-        "es6": true,
-    },
-    "rules": {
-        "no-plusplus" : 0,
-    }
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "env": {
+    "browser": true,
+    "es6": true,
+  },
+  "rules": {
+    "no-plusplus": 0,
+    "no-param-reassign": [2, {
+      "props": false,
+    }],
+  }
 };
